dedfac22ff1d7001b4433d5ffab2ffb37ea74dc7
BT_1N3_18 <sample@bths.edu>
2025-03-20 10:58:59 -0400

    changed coordinates for some text + played around with visuals

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -13,54 +13,59 @@ import java.io.IOException;
 
 public class DisplayPanel extends JPanel implements ActionListener {
     private String message;
-    private JButton button;
-    private JButton submit;
-    private BufferedImage goomba;
+    private JButton play;
+    private JButton rules;
+    private BufferedImage mastermind;
     private JTextField textField;
 
-    public DisplayPanel() {
-        message = "This is the display panel!";
-        button = new JButton("Click me");
-        button.addActionListener(this);
-        add(button);
+    //TODO: Create a welcome panel and then have mastermind board show up when button pressed
+    //TODO: Will add different button reactions and text visuals
 
-        try {
-            goomba = ImageIO.read(new File("src\\goomba.png"));
+
+    public DisplayPanel() {
+        message = ("Welcome to Mastermind!");
+        play = new JButton("PLAY");
+        //button = new JButton("Rules of the Game");
+        play.addActionListener(this);
+        add(play);
+
+       /* try {
+            mastermind = ImageIO.read(new File("src\\mastermind.png"));
         } catch (IOException e) {
             System.out.println(e.getMessage());
-        }
+        }*/
 
         textField = new JTextField(10);
         add(textField);
 
-        submit = new JButton("SUBMIT");
-        submit.addActionListener(this);
-        add(submit);
+        rules = new JButton("RULES");
+        rules.addActionListener(this);
+        add(rules);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.setFont(new Font("Arial", Font.BOLD, 40));
         g.setColor(Color.RED);
-        g.drawString(message, 50, 30);
-        button.setLocation(50, 100);
-        g.drawImage(goomba, 200, 50, null);
+        g.drawString(message, 300, 100);
+        play.setLocation(300, 300);
+        g.drawImage(mastermind, 200, 50, null);
         g.setColor(Color.BLACK);
-        g.drawString("goomba!", 200, 110);
-        textField.setLocation(50, 60);
-        submit.setLocation(50, 130);
+        //g.drawString("goomba!", 200, 110);
+        //textField.setLocation(50, 60);
+        rules.setLocation(300, 330);
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() instanceof JButton) {
             JButton casted = (JButton) e.getSource();
-            if (casted == button) {
+            if (casted == play) {
                 message = "CLICKED!";
                 repaint();
             }
 
-            if (casted == submit) {
+            if (casted == rules) {
                 // obtain string from text field
                 String enteredText = textField.getText();
 
