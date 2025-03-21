bdcfbe6010557d1194133cb3051552e99b2f959b
BT_1N3_17 <k@g.com>
2025-03-21 10:20:05 -0400

    some button functionality

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -13,7 +13,9 @@ import java.io.IOException;
 
 public class DisplayPanel extends JPanel implements ActionListener {
     private String message;
+    private String rulesText;
     private JButton play;
+    private JButton backToHome;
     private JButton rules;
     private BufferedImage mastermind;
     private JTextField textField;
@@ -25,9 +27,15 @@ public class DisplayPanel extends JPanel implements ActionListener {
     public DisplayPanel() {
         message = ("Welcome to Mastermind!");
         play = new JButton("PLAY"); //ability to put in png for icon
+        rulesText = ("");
         //button = new JButton("Rules of the Game");
         play.addActionListener(this);
         add(play);
+
+        backToHome = new JButton("Back");
+        backToHome.addActionListener(this);
+        add(backToHome);
+        backToHome.setVisible(false);
 
        /* try {
             mastermind = ImageIO.read(new File("src\\mastermind.png"));
@@ -51,13 +59,16 @@ public class DisplayPanel extends JPanel implements ActionListener {
         g.drawString(message, 150, 100);
         play.setLocation(400, 400);
         play.setSize(200, 70);
+        g.setFont(new Font("Arial", Font.BOLD, 20));
 
+        g.drawString(rulesText,500,500);
         g.drawImage(mastermind, 200, 50, null);
         g.setColor(Color.BLACK);
-        //g.drawString("goomba!", 200, 110);
-        //textField.setLocation(50, 60);
         rules.setLocation(400, 480);
         rules.setSize(200, 70);
+
+        backToHome.setLocation(50,100);
+        backToHome.setSize(200,70);
 
     }
 
@@ -70,14 +81,24 @@ public class DisplayPanel extends JPanel implements ActionListener {
             }
 
             if (casted == rules) {
-                // obtain string from text field
-                String enteredText = textField.getText();
-
                 // update message to the entered text
-                message = enteredText;
+                message = "Rules";
+                rulesText = "Hello Rules are here";
+                rules.setVisible(false);
+                play.setVisible(false);
+                backToHome.setVisible(true);
 
-                // refresh the screen so that the updated message gets displayed
                 repaint();
+            }
+
+            if (casted == backToHome){
+                message = "Welcome";
+                rulesText = "";
+                rules.setVisible(true);
+                play.setVisible(true);
+                backToHome.setVisible(false);
+                repaint();
+
             }
         }
     }
